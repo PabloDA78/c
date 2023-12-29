134cd538dfe0d394a3c02eec6e1631b0cc5637b5
PabloDA78 <demederosp@gmail.com>
2023-12-28 23:51:22 -0600

    Actualizando tutoriales de data structures

--- data_Structures_Tutorial/main.cpp
+++ data_Structures_Tutorial/main.cpp
@@ -1,13 +1,45 @@
 #include <iostream>
 #include <string> // Include the necessary header for string
+#include <iomanip>
 
 using std::string;
 using namespace std;
 
 int main(){
     
+    //A Data structure is an organized collection of data of any kind
+
+
+    /* //WORKING WITH ARRAYS
+        int size; //User defines the amount of elements in the array
+        cout<<"How many months do you want to enter? "<<endl;
+        cin>>size;
+        float *monthsArray=new float[size]; //{100,220,300,0,200,250}; //Monthly savings amount
+        float total=0;
+    //Populating the array
+    //Calculating the sum of the array
+
+        for (int i = 0; i < size; i++)
+        {
+            cout<<"Enter the amount saved during month "<<i+1<<":"<<endl;
+            cin>>monthsArray[i];
+            total+=monthsArray[i];
+        }
+        delete [] monthsArray;
+        monthsArray=NULL;
+
+        float average=total/size;
+        float inTwoYears=average*24;
+
+    //Outputs
+
+        cout<<"Total is: "<<total<<endl;
+        cout<<"Average is: "<<fixed<<setprecision(2)<<average<<endl;
+        cout<<"In 2 Years is: "<<fixed<<setprecision(2)<<inTwoYears<<endl; */
     
+    //LINKED LISTS
     
+        
 
 
 }
