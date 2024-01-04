14f9909b3fd1ddbcde87a11879407fe42c90d352
PabloDA78 <demederosp@gmail.com>
2024-01-03 18:44:30 -0600

    Actualizando tutoriales de data structures

--- data_Structures_Tutorial/main.cpp
+++ data_Structures_Tutorial/main.cpp
@@ -2,12 +2,24 @@
 #include <string> // Include the necessary header for string
 #include <iomanip>
 #include <stack>
+#include <queue>
+#include <map>
+#include <list>
+
 
 using std::string;
-using namespace std;
+using namespace std; //Using map, string and cout
 
+//Creating the Class Node2 (Doubly Linked Lists)
 
-//Creating the class node
+    class Node2{
+        public:
+            int Value;
+            Node2* Next; //Pointer to the next value
+            Node2* Previous; //Pointer to the previous value
+    };
+
+//Creating the class node (Linked Lists)
         class Node{
         public:
             int Value;//Value of the node
@@ -20,6 +32,9 @@ void insertAtTheFront(Node** head,int newValue);
 void insertAtTheEnd(Node** head,int newValue);
 void insertAfter(Node*previous, int newValue);
 void printStackElements(stack <int> stack);
+void printQueue(queue <int> queue);
+void printForward(Node2* head2); //Used in doubly linked lists
+void printBackward(Node2* tail); //Used in doubly linked lists
 
 int main(){
     
@@ -138,7 +153,156 @@ int main(){
         printStackElements(numbersStack);//calling the printing frunction
 
 
+    //QUEUE DATA STRUCTURE.  FIFO/FCFS
+    //FIFO = First In, First Out, FCFS= First come firts served
+    //Used for printers, ordered transactions, any real life queue
+    //You have to include queue at the top of the program 
+    //Funcitons: push, size, front, back 
 
+        queue <int>  myQueue;
+
+        //Adding elements to end of the queue
+        myQueue.push(1);
+        myQueue.push(2);
+        myQueue.push(3);
+        myQueue.push(4);
+
+
+        //Other Functions
+        cout<<"Queue size is: "<<myQueue.size()<<endl;
+        cout<<"First element is: "<<myQueue.front()<<endl;
+        cout<<"Last element is: "<<myQueue.back()<<endl;
+
+        //Printing the Queue
+        cout<<"My Queue: "<<endl;
+        printQueue(myQueue);
+
+        //Removing elements from the front of the queue
+        myQueue.pop(); 
+        cout<<"We removed the first element that came in (1)"<<endl;
+        printQueue(myQueue);
+
+    //MAP DATA STRUCTURE IN C++  / Dictionary in other languages
+    //Key Value Pairs of Data
+    //Associate  A Key with a Value/Values .  A key is always a simple data type , the value can be a complex data type list/array of values
+    //You have to include map at the top of the program 
+    //Use unique keys
+    //A Map orders keys by ascending order (a,b,c   1,2,3)
+    //To have an unorderd map you use #include <unordered_map> and the type is unordered_map. Elements appear as you input them
+
+        map <string, string>  myDictionary;//map <key type, value type> nameOfMap 
+
+        //Insert elements in the dictionary
+
+        myDictionary.insert(pair<string,string>("apple","manzana"));//pair<data type,data type>("key","related value");
+        myDictionary.insert(pair<string,string>("banana","platano, banana"));//relacionando key banana con dos valores platano y banana en un mismo string
+        myDictionary.insert(pair<string,string>("orange","naranja"));
+        myDictionary.insert(pair<string,string>("avocado","aguacate"));//lo va poner como 2do por orden alfabeticos
+        
+        //Printing the dictionary
+
+        for (auto pair : myDictionary) //For every pair in my dictionary ( g++ -std=c++11 main.cpp -o exec to compile with auto)
+        {
+            cout<<pair.first<<" - "<<pair.second<<endl; //Printing the first part (key) and the second part whichi is the value
+        }
+
+        //Access specific elements
+        
+        myDictionary["orange"]="Naranja";//mapName["specific key"] = "Value"
+        for (auto pair : myDictionary) //Refleja el cambio a mayuscula de "Naranja"
+        {
+            cout<<pair.first<<" - "<<pair.second<<endl; 
+        }
+        cout<<"Printing specificaly: "<<endl;
+        cout<<myDictionary["orange"]<<endl;
+
+        //Size of collection
+        cout<<myDictionary.size()<<endl;
+
+        //Delete all elements
+        myDictionary.clear();
+        cout<<myDictionary.size()<<endl;
+
+        //Pairing Keys with Comples Values
+        //Pokedex: Keys- Name of Pokemon, Values - List of Attacks
+        //Included <list>
+
+        //Relating a string with a list of string
+        map <string,list<string>>  pokedex;
+        //Creating a list of strings
+
+        list<string> pikachuAttacks{"thunder shock","tail whip","quick attack"};
+        list<string> charmanderAttacks{"fire ball","scary face"};
+        list<string> squirtleAttacks{"water ball","squirt"};
+
+        pokedex.insert(pair<string,list<string>>("Pikachu",pikachuAttacks));
+        pokedex.insert(pair<string,list<string>>("Charmander",charmanderAttacks));
+        pokedex.insert(pair<string,list<string>>("Squirtle",squirtleAttacks));
+        
+        //Since it is a list we need a nested for loop to iterate through the list
+
+        for (auto pair : pokedex) 
+        {
+            cout<<pair.first<<" - ";
+            for (auto attack: pair.second)
+            {
+                cout<<attack<<", "; 
+            }
+            cout<<endl;  
+        }
+    
+    
+    //Doubly Linked Lists 
+    //A doubly linked list has a node with a value, a pointer to the next node and a pointer to the previous node
+    //Uses more memory
+    
+
+    //Creating the first node
+
+    Node2* head2; //using head 2 just because head was already created for the single linked list
+    Node2* tail;
+
+    Node2* node = new Node2(); //Creating a node pointer called "node" of class Node2 and allocating memory
+    node->Value=4; //Setting the value for the node
+    node->Next=nullptr; 
+    node->Previous=nullptr; //Since there is no other node it is the first and last node... points to NULL in both ways
+    head2=node;
+    tail=node;
+
+    //Adding the second node to the list
+    node = new Node2(); //Allocate space for a new node, we use the same varialbe because we have added the first node to the list and we dont need it to point there any longer
+    node->Value=5;
+    node->Next=nullptr;// Since this is the last created node it points to the null pointer
+    node->Previous=tail; //Pointing to the previous tail
+    tail->Next=node; //Previous tail now has to point to the new node
+    tail=node;//Make the node the new tail
+
+    //To add futher nodes.. you can copy the code for the second node and just change the values
+    node = new Node2(); 
+    node->Value=6;
+    node->Next=nullptr;
+    node->Previous=tail; 
+    tail->Next=node; 
+    tail=node;
+
+    //Traversing the list to print elements
+    cout<<"Printing forward"<<endl;
+    printForward(head2);
+    cout<<"Printing backward"<<endl;
+    printBackward(tail);
+
+//TREE DATA STRUCTURE
+
+
+    
+
+
+
+
+
+
+
+        
 
 }
 
@@ -199,6 +363,27 @@ void printStackElements(stack <int> stack){
         stack.pop();
     }
 }
-
-
+void printQueue(queue <int> queue){
+    while(!queue.empty()){
+        cout<<queue.front()<<endl;
+        queue.pop();
+    }
+    cout<<endl;    
+}
+void printForward(Node2* head2){
+    Node2* traverser=head2; //It will go from node to node and print the value of the current node
+    while (traverser!=nullptr)
+    {
+        cout<<traverser->Value<<endl;
+        traverser=traverser->Next;
+    }
+}
+void printBackward(Node2* tail){
+    Node2* traverser=tail; //It will go from node to node and print the value of the current node
+    while (traverser!=nullptr)
+    {
+        cout<<traverser->Value<<endl;
+        traverser=traverser->Previous;
+    }
+}
 
